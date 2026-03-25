ed2dc7f5b5d67706fe6abce500405b8c2677d757
Snowy <87611218+Snowy893@users.noreply.github.com>
2026-03-25 14:10:34 -0400

    Random sounds

--- origins/blazeborn.lua
+++ origins/blazeborn.lua
@@ -2,6 +2,12 @@ local origins = require "origins.origins"
 
 local blazeborn = origins.new("origins:blazeborn")
 
+blazeborn.sound = {
+    obj = sounds["minecraft:entity.blaze.burn"],
+}
+
+blazeborn.emissiveBuffer = 0
+
 function blazeborn.emissive()
     return player:isOnFire()
 end
--- origins/enderian.lua
+++ origins/enderian.lua
@@ -2,6 +2,10 @@ local origins = require "origins.origins"
 
 local enderian = origins.new("origins:enderian")
 
+enderian.sound = {
+    obj = sounds["minecraft:entity.enderman.ambient"]:volume(0.75),
+}
+
 enderian.emissive = true
 
 enderian:init()
