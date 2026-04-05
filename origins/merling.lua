25e4673114a851ec578dd9fb7e156ffd2370eb23
Snowy <87611218+Snowy893@users.noreply.github.com>
2026-04-04 20:21:07 -0400

    Added merling fins, moth wing attempt

---
+++ origins/merling.lua
@@ -0,0 +1,39 @@
+local origins = require "origins.origins"
+local squapi = require "lib.thirdparty.SquAPI"
+
+local merling = origins.new("merling")
+
+local headFins = models.model.root.Head.merlingFins
+local backFin = models.model.root.Body.merlingBackFin
+
+merling.parts = { headFins, backFin }
+
+merling.squishy = {
+    squapi.ear:new(headFins.merlingLeftFin, headFins.merlingRightFin,
+        0.25,
+        true,
+        0.1,
+        true,
+        1200,
+        0.1,
+        0.8,
+        5
+    ),
+    squapi.tail:new({ backFin },
+        4,
+        0.0,
+        nil,
+        0.0,
+        0.2,
+        nil,
+        nil,
+        nil,
+        0.008,
+        0.0,
+        0,
+        0,
+        0
+    ),
+}
+
+merling:register()
